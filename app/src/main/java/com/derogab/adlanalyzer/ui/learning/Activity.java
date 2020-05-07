b5c35a8c0f16c33c52ad851e67a8f7231c79fc7f
Gabriele De Rosa <derosagabriele@outlook.it>
2020-05-07 04:32:34 +0200

    LearningFragment

---
+++ app/src/main/java/com/derogab/adlanalyzer/ui/learning/Activity.java
@@ -0,0 +1,55 @@
+package com.derogab.adlanalyzer.ui.learning;
+
+public class Activity {
+
+    private String name;
+    private int seconds;
+    private double frequency;
+    private String[] sensors;
+
+    public Activity(String name) {
+        this.name = name;
+    }
+
+    public Activity(String name, int seconds) {
+        this.name = name;
+        this.seconds = seconds;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public double getFrequency() {
+        return seconds;
+    }
+
+    public void setFrequency(double frequency) {
+        this.frequency = frequency;
+    }
+
+    public String[] getSensors() {
+        return sensors;
+    }
+
+    public void setSensors(String[] sensors) {
+        this.sensors = sensors;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
--- app/src/main/java/com/derogab/adlanalyzer/ui/learning/LearningFragment.java
+++ app/src/main/java/com/derogab/adlanalyzer/ui/learning/LearningFragment.java
@@ -1,9 +1,11 @@
 package com.derogab.adlanalyzer.ui.learning;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -25,40 +27,99 @@ import java.util.ArrayList;
 
 public class LearningFragment extends Fragment {
 
+    public static final String TAG = "LearningFragment";
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_learning, container, false);
-        MaterialSpinner spinner = (MaterialSpinner) root.findViewById(R.id.spinner);
+        final View root = inflater.inflate(R.layout.fragment_learning, container, false);
+        MaterialSpinner activity_selector = (MaterialSpinner) root.findViewById(R.id.activity_selector);
+
+        final Button startLearning = root.findViewById(R.id.fragment_learning_start_button);
+
+
 
         JSONArray activities;
-        ArrayList<String> act_list = new ArrayList<>();
+        ArrayList<Activity> act_list = new ArrayList<>();
         try {
 
             activities = new JsonSource("https://pastebin.com/raw/bs6RK1Wv")
                                     .getJSON()
                                     .getJSONArray("activities");
 
-            if (activities != null) {
+            for(int i = 0 ; i < activities.length(); i++) {
 
-                for(int i = 0 ; i < activities.length() ; i++) {
+                // Get params
+                JSONObject params = activities.getJSONObject(i);
 
-                    // Get params
-                    JSONObject params = activities.getJSONObject(i);
+                // Create activity
+                Activity tmp = new Activity(params.getString("name"),
+                        params.getInt("time"));
 
-                    // Add activities to  Spinner list
-                    act_list.add(params.getString("name"));
+                JSONArray sensors_json = params.getJSONArray("sensors");
+                String sensors_array[] = new String[sensors_json.length()];
+                for (int j = 0; j < sensors_json.length(); j++) {
+                    sensors_array[j] = sensors_json.getString(j);
                 }
+                tmp.setSensors(sensors_array);
 
+                // Add activities to  Spinner list
+                act_list.add(tmp);
             }
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
+        activity_selector.setItems(act_list);
 
-        spinner.setItems(act_list);
+
+        Activity first_selected = (Activity) activity_selector.getItems().get(activity_selector.getSelectedIndex());
+        init_config(root, first_selected);
+
+
+        activity_selector.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<Activity>() {
+
+            @Override public void onItemSelected(MaterialSpinner view, int position, long id, Activity item) {
+
+            init_config(root, item);
+
+            }
+
+        });
+
+
 
         return root;
     }
+
+    private void init_config(View v, Activity item){
+
+        final TextView timeValue = v.findViewById(R.id.fragment_learning_config_seconds_value);
+        final TextView frequencyValue = v.findViewById(R.id.fragment_learning_config_frequency_value);
+        final TextView sensorsValue = v.findViewById(R.id.fragment_learning_config_sensors_value);
+
+        timeValue.setText(item.getSeconds() + "sec");
+        frequencyValue.setText(item.getFrequency() + "bitrate");
+
+        String[] sensors = item.getSensors();
+        String sensors_str = "";
+
+        if(sensors != null){
+
+            for (int i = 0; i < sensors.length; i++) {
+                sensors_str += sensors[i];
+
+                if (i != sensors.length-1) sensors_str += ", ";
+
+            }
+
+
+
+            sensorsValue.setText(sensors_str);
+        }
+
+    }
+
+
 }
