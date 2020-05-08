dcdc35f7acac09ad105a6e43416c4c1fda54e967
Gabriele De Rosa <derosagabriele@outlook.it>
2020-05-08 03:05:42 +0200

    Sensors

--- app/src/main/java/com/derogab/adlanalyzer/MainActivity.java
+++ app/src/main/java/com/derogab/adlanalyzer/MainActivity.java
@@ -1,6 +1,10 @@
 package com.derogab.adlanalyzer;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -10,7 +14,9 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,37 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            Log.d(TAG, "[GYROSCOPE] X: "+x+", Y: "+y+", Z: "+z);
+
+            // @TODO: send gyroscope data to server
+
+        }
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            Log.d(TAG, "[ACCELEROMETER] X: "+x+", Y: "+y+", Z: "+z);
+
+            // @TODO: send accelerometer data to server
+
+        }
+
     }
 
 }
--- app/src/main/java/com/derogab/adlanalyzer/ui/learning/LearningFragment.java
+++ app/src/main/java/com/derogab/adlanalyzer/ui/learning/LearningFragment.java
@@ -1,5 +1,10 @@
 package com.derogab.adlanalyzer.ui.learning;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.speech.tts.TextToSpeech;
@@ -13,11 +18,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.derogab.adlanalyzer.JsonSource;
 import com.derogab.adlanalyzer.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.jaredrummler.materialspinner.MaterialSpinner;
 
@@ -28,34 +37,46 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Locale;
 
+import static android.content.Context.SENSOR_SERVICE;
+
 public class LearningFragment extends Fragment {
 
     private static final String TAG = "LearningFragment";
 
     private static final String ACTIVITY_SELECTED_INDEX = "activity_selected_index";
 
-
     private TextToSpeech textToSpeech;
-    private CountDownTimer countDownTimer;
+    private CountDownTimer preparationTimer, activityTimer;
 
     private TextView timeValue, frequencyValue, sensorsValue;
 
     private int activitySelectedIndex;
 
+    private SensorManager sensorManager;
+
+    FragmentActivity mContext;
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
+        mContext = this.getActivity();
 
         // Layout elements
         final View root = inflater.inflate(R.layout.fragment_learning, container, false);
-        final MaterialSpinner activity_selector = (MaterialSpinner) root.findViewById(R.id.activity_selector);
+        final MaterialSpinner activitySelector = (MaterialSpinner) root.findViewById(R.id.activity_selector);
         final Button startLearning = root.findViewById(R.id.fragment_learning_start_button);
 
 
+
+
+        // SensorManager init
+        sensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
+
         // Get activities list
         JSONArray activities;
-        ArrayList<Activity> act_list = new ArrayList<>();
+        ArrayList<Activity> activitiesList = new ArrayList<>();
         try {
 
             activities = new JsonSource("https://pastebin.com/raw/bs6RK1Wv")
@@ -69,18 +90,18 @@ public class LearningFragment extends Fragment {
 
                 // Create activity
                 Activity tmp = new Activity(params.getString("name"),
-                                params.getInt("time"));
+                                            params.getInt("time"));
 
                 // Convert data sensors format
-                JSONArray sensors_json = params.getJSONArray("sensors");
-                String sensors_array[] = new String[sensors_json.length()];
-                for (int j = 0; j < sensors_json.length(); j++) {
-                    sensors_array[j] = sensors_json.getString(j);
+                JSONArray sensorsJson = params.getJSONArray("sensors");
+                String sensorsArray[] = new String[sensorsJson.length()];
+                for (int j = 0; j < sensorsJson.length(); j++) {
+                    sensorsArray[j] = sensorsJson.getString(j);
                 }
-                tmp.setSensors(sensors_array);
+                tmp.setSensors(sensorsArray);
 
                 // Add activities to  Spinner list
-                act_list.add(tmp);
+                activitiesList.add(tmp);
             }
 
         } catch (JSONException e) {
@@ -88,28 +109,28 @@ public class LearningFragment extends Fragment {
         }
 
         // Add activities list to the spinner list
-        activity_selector.setItems(act_list);
+        activitySelector.setItems(activitiesList);
 
         // Set all saved data
         if(savedInstanceState != null){
             Log.d(TAG, "Restore InstanceState...");
             activitySelectedIndex = savedInstanceState.getInt(ACTIVITY_SELECTED_INDEX);
 
-            if (activitySelectedIndex < activity_selector.getItems().size()) {
-                activity_selector.setSelectedIndex(activitySelectedIndex);
+            if (activitySelectedIndex < activitySelector.getItems().size()) {
+                activitySelector.setSelectedIndex(activitySelectedIndex);
             }
 
         }
 
         // Get first activity selected index
-        activitySelectedIndex = activity_selector.getSelectedIndex();
+        activitySelectedIndex = activitySelector.getSelectedIndex();
 
         // First selected config
-        Activity first_selected = (Activity) activity_selector.getItems().get(activitySelectedIndex);
+        Activity first_selected = (Activity) activitySelector.getItems().get(activitySelectedIndex);
         initConfig(root, first_selected);
 
         // Set listener on Spinner select change
-        activity_selector.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<Activity>() {
+        activitySelector.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<Activity>() {
 
             @Override public void onItemSelected(MaterialSpinner view, int position, long id, Activity item) {
 
@@ -124,22 +145,22 @@ public class LearningFragment extends Fragment {
         textToSpeech = new TextToSpeech(getContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                if (status == TextToSpeech.SUCCESS) {
-                    Log.d(TAG, "Current Locale: "+getString(R.string.current_lang));
+            if (status == TextToSpeech.SUCCESS) {
+                Log.d(TAG, "Current Locale: "+getString(R.string.current_lang));
 
-                    int ttsLang = textToSpeech.setLanguage(new Locale(getString(R.string.current_lang)));
+                int ttsLang = textToSpeech.setLanguage(new Locale(getString(R.string.current_lang)));
 
-                    if (ttsLang == TextToSpeech.LANG_MISSING_DATA
-                            || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
-                        Log.e(TAG, "[TTS] The Language is not supported!");
-                    } else {
-                        Log.i(TAG, "[TTS] Language Supported.");
-                    }
-                    Log.i(TAG, "[TTS] Initialization success.");
+                if (ttsLang == TextToSpeech.LANG_MISSING_DATA
+                        || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Log.e(TAG, "[TTS] The Language is not supported!");
                 } else {
-
-                    Log.e(TAG, "[TTS] Initialization failed!");
+                    Log.i(TAG, "[TTS] Language Supported.");
                 }
+                Log.i(TAG, "[TTS] Initialization success.");
+            } else {
+
+                Log.e(TAG, "[TTS] Initialization failed!");
+            }
             }
         });
 
@@ -147,8 +168,8 @@ public class LearningFragment extends Fragment {
         startLearning.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "button clicked. ");
-                countDownTimer.start();
+            Log.d(TAG, "button clicked. ");
+            preparationTimer.start();
             }
         });
 
@@ -162,8 +183,6 @@ public class LearningFragment extends Fragment {
         Log.d(TAG, "Saving InstanceState...");
         outState.putInt(ACTIVITY_SELECTED_INDEX, activitySelectedIndex);
     }
-
-
 
     @Override
     public void onDestroy() {
@@ -184,25 +203,52 @@ public class LearningFragment extends Fragment {
         frequencyValue.setText(item.getFrequency() + " bit/s");
 
         String[] sensors = item.getSensors();
-        String sensors_str = "";
+        String sensorsStr = "";
 
         if(sensors != null){
 
             for (int i = 0; i < sensors.length; i++) {
-                sensors_str += sensors[i];
+                sensorsStr += sensors[i];
 
-                if (i != sensors.length-1) sensors_str += ", ";
+                if (i != sensors.length-1) sensorsStr += ", ";
 
             }
-            sensorsValue.setText(sensors_str);
+            sensorsValue.setText(sensorsStr);
         }
 
-        countDownTimer = new CountDownTimer(item.getSeconds() * 1000, 1000) {
+        preparationTimer = new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.d(TAG, "seconds remaining before start: " + millisUntilFinished / 1000);
+
+                if ((millisUntilFinished / 1000) == 3) {
+
+                    int speechStatus = textToSpeech.speak("The activity is about to begin", TextToSpeech.QUEUE_FLUSH, null);
+
+                    if (speechStatus == TextToSpeech.ERROR) {
+                        Log.e(TAG, "[TTS] Error in converting Text to Speech!");
+                    }
+
+                }
+
+                timeValue.setText("-" + (millisUntilFinished / 1000) + " sec");
+
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d(TAG, "preparation timer done!");
+
+                sensorManager.registerListener((SensorEventListener) mContext, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+                sensorManager.registerListener((SensorEventListener) mContext, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
+                activityTimer.start();
+            }
+        };
+
+        activityTimer = new CountDownTimer(item.getSeconds() * 1000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 Log.d(TAG, "seconds remaining: " + millisUntilFinished / 1000);
-
-                timeValue.setText("" + (millisUntilFinished / 1000) + " sec");
 
                 if (millisUntilFinished / 1000 % 5 == 0) {
 
@@ -214,11 +260,15 @@ public class LearningFragment extends Fragment {
 
                 }
 
+                timeValue.setText("" + (millisUntilFinished / 1000) + " sec");
+
             }
 
             @Override
             public void onFinish() {
-                Log.d(TAG, "timer done!");
+                Log.d(TAG, "activity timer done!");
+
+                sensorManager.unregisterListener((SensorEventListener) mContext);
 
                 int speechStatus = textToSpeech.speak("done!", TextToSpeech.QUEUE_FLUSH, null);
 
